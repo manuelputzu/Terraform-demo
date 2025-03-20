89e5387c13dde09744208d29f87ceca73509cec3
manuelputzu <manuel.putzu@gmx.de>
2025-03-20 11:29:19 +0100

    Declare variables in variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vpn_ip" {}
+
+variable "vpc_id" {}
+
+variable subnet {}
+
+variable app_port {}
+
+variable ssh_port {}
+
+variable instance_type {}
+
+variable ami {}
+
+variable profile-id {} 
